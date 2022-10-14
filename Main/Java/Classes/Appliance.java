eeb9cf8ed8f84307620ae9eb9f14d4de94b877f2
Denisa Rissa <57639793+RissaDR@users.noreply.github.com>
2022-10-14 23:44:09 +0200

    Update Appliance.java
    Add activity reference to appliance class constructor.

--- Main/Java/Classes/Appliance.java
+++ Main/Java/Classes/Appliance.java
@@ -2,9 +2,15 @@ package worldOfZuul.Main.Java.Classes;
 
 public class Appliance {
     private final String name; // Name of the appliance
+    private Activity activity; // Reference to associated activity
 
-    public Appliance(String name) {
+    public Appliance(String name, Appliance activity) {
         this.name = name;
+        this.activity = activity;
+    }
+
+    public Activity getApplianceActivity() {
+        return activity;
     }
 
     public String getApplianceName() {
