093c9f24deb091e8de30faf401684ee3ffd34758
Denisa <denisarissa@gmail.com>
2022-10-14 21:58:03 +0200

    Created Appliance class with name attribute
    This class represents an appliance.

---
+++ Main/Java/Classes/Appliance.java
@@ -0,0 +1,13 @@
+package worldOfZuul.Main.Java.Classes;
+
+public class Appliance {
+    private final String name; // Name of the appliance
+
+    public Appliance(String name) {
+        this.name = name;
+    }
+
+    public String getApplianceName() {
+        return name;
+    }
+}
