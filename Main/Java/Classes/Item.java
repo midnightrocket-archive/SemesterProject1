7d3b5df69e934b109fa3b7a7c42dfae5f3d7d372
Denisa <denisarissa@gmail.com>
2022-10-14 22:45:09 +0200

    Create Item class with name attribute
    This represents an item.

---
+++ Main/Java/Classes/Item.java
@@ -0,0 +1,13 @@
+package worldOfZuul.Main.Java.Classes;
+
+public class Item {
+    private final String name; // Name of the item
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getItemName() {
+        return name;
+    }
+}
