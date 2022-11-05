69fa6fa4078468be53a00db609b6f07d13ef8e1a
MidnightRocket <45201103+MidnightRocket@users.noreply.github.com>
2022-11-05 21:42:50 +0100

    Rewrite of Player class, now greatly simplified
    - Removed the `use()` method, as it is not the role of player. The use function in player is way to convoluted, and should be reimplemented. This is yet to be done. See Know Regression below.
    - Removed property `inventoryReference` as Game currently handles all operations on inventory.
    - Added property `playerName` to be able to give player a name.
    - Player currently does not have any role besides the name.

--- Main/Java/Classes/Player.java
+++ Main/Java/Classes/Player.java
@@ -7,26 +7,11 @@ import worldOfZuul.Main.Java.Game;
 import java.util.ArrayList;
 
 public class Player {
-    Room currentRoom;
-    private Inventory inventory;
     private String playerName;
 
 
     public Player(String playerName) {
         this.playerName = playerName;
-    }
-
-
-    public void addItemToInventory(Item item) {
-        inventory.addItem(item);
-    }
-
-    public void removeItemFromInventory(Item item) {
-        inventory.removeItem(item);
-    }
-
-    public ArrayList<Item> getInventory() {
-        return inventory.getInventoryList();
     }
 
     public String getPlayerName() {
