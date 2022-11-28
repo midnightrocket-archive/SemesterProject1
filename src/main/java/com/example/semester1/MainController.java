3f1416f8bf2ee9ed606cff065607cfae8f913c37
TidosDK <28749155+TidosDK@users.noreply.github.com>
2022-11-28 12:30:43 +0100

    File names refactored

--- src/main/java/com/example/semester1/MainController.java
+++ src/main/java/com/example/semester1/MainController.java
@@ -3,7 +3,7 @@ package com.example.semester1;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
